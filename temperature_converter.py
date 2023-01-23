740fcc608a9244232768e3793f5e45d7efa3aa73
Efhn <113469356+Efhn@users.noreply.github.com>
2023-01-23 19:24:14 +0000

    temp change 2

--- temperature_converter.py
+++ temperature_converter.py
@@ -5,6 +5,7 @@ A temperature converter from Celsius to Farenheit
 # Formula: F = (C * 9/5) + 32
 TEMP_CONSTANT_P1 = 9/5
 TEMP_CONSTANT_P2 = 32
+
 celsius = float(input('Enter the temperature in Celsius: '))
 feranheit = celsius * TEMP_CONSTANT_P1 + TEMP_CONSTANT_P2
 print('The equivalent temperature of', 'Celsius in Farenheit is', feranheit)
