130a0895e6c2d2ab51b2eba38e939afa2fd8687c
Efhn <113469356+Efhn@users.noreply.github.com>
2023-01-25 18:54:42 +0000

    save notes

--- Module3/my_strings.py
+++ Module3/my_strings.py
@@ -11,3 +11,6 @@ print('My name is', name, 'and my school is', school)
 print('My name is {0} and my school is {1}'.format(name, school))
 
 print('At {1}, my name is {0} and my school is {1}'.format(name, school))
+
+# Format integers, floats, strings, etc
+print('{0} {1} cost ${2}'.format(6, 'bananas', 1.56))
