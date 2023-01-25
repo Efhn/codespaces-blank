089a1cdf2aa33172c2252b4d3dca28da0e2334a3
Efhn <113469356+Efhn@users.noreply.github.com>
2023-01-25 19:07:30 +0000

    save notes

--- Module3/README.md
+++ Module3/README.md
@@ -3,4 +3,15 @@
 ## Strings
 Is a collection of charcters
 
-Use String `.format()` Method
+- Use String `.format()` Method
+```python
+full_name = '{0} {1}'.format('Waldo', 'Weber')
+```
+- `f-string` for your new string format
+```
+- `f-string` for your new string format
+```python
+first_name = 'Waldo'
+last_name = 'Weber'
+full_name = f'{first_name} {last_name}'
+```
--- Module3/my_strings.py
+++ Module3/my_strings.py
@@ -14,3 +14,21 @@ print('At {1}, my name is {0} and my school is {1}'.format(name, school))
 
 # Format integers, floats, strings, etc
 print('{0} {1} cost ${2}'.format(6, 'bananas', 1.56))
+
+# Keyword arguments
+print('{quantity} {item} cost ${price}'.format(
+    item = 'bananas',
+    quantity = 6,
+    price = 1.56))
+
+# Use f-strings
+item = 'bananas'
+quantity = 6,
+price = 1.56
+print(f'{quantity} {item} cost ${price}')
+
+# Task: Calculate the area of a rectangle
+length = 10.5
+width = 5.21
+area = length * width
+print(f'The area of a rectangle with {length} length and {width} width is:{area}')
