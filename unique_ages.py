d8c91794a804b253ede27ba846ca3bd556b6a058
Efhn <113469356+Efhn@users.noreply.github.com>
2023-02-06 19:23:28 +0000

    notes

--- unique_ages.py
+++ unique_ages.py
@@ -4,15 +4,25 @@ Create a file that ask the user to enter 5 integers (age).
 Store information in a `list`, then, print all values
 '''
 
-# TODO: loop to take user input
-ages = [] # empty list
-for index in range(5):
+num_ages = int(input('How many integers do you want to capture:'))
+# loop to take user input
+# ages = [] # empty list
+for index in range(num_ages):
+    ages = [] # empty list
     age = int(input('Enter your age'))
-    # TODO: Save values in a list
+    # Save values in a list
     ages.append(age)
 
-# TODO: Loop to print input
+# Loop to print input
 for age in ages:
     print(age)
 
 # TODO: Print the average age
+total = 0
+for age in ages:
+    total += age
+
+print(f'Total of ages is {total}')
+print(f'Average {total/len(ages)}')
+print(f'Max age {max(ages)}')
+print(f'Minimum age {min(ages)}')
