b1a782b476110f8400a6e0289f7fc8c46eb65d5d
Efhn <113469356+Efhn@users.noreply.github.com>
2023-02-08 20:21:04 +0000

    radiation notes

--- Module 4/radiation.py
+++ Module 4/radiation.py
@@ -5,6 +5,7 @@ Display the description of some radiation given its wavelength
 
 import sys
 
+INVALID = -1
 RADIO = 0 
 MICROWAVE = 1
 INFRARED = 2
@@ -12,34 +13,46 @@ VISIBLE = 3
 ULTRAVIOLET = 4
 X_RAYS = 5
 GAMMA = 6
+
+rad_level = INVALID # initialize it to zero
 RADIATION_TYPES = ['Radio Waves', 'Microwaves', 'Infrared',
-                   'Visible light', 'Ultraviolet', 'X-Rays', 'Gamma Rays' ]
-# TODO: Capture the frequency
-frequency = float(input('Enter the frequency. ex: 3e12 '))
+                    'Visible light', 'Ultraviolet', 'X-Rays',
+                    'Gamma Rays']
 
-# TODO: Display the description of the radiation
-# Less than or equal 3e9: Radio waves
-if frequency <= 3e9:
-    rad_level = RADIO
-# More than 3e9 and less than or equal 3e11: Microwaves
-elif frequency > 3e9 and frequency <= 3e11:
-    rad_level = MICROWAVE
-# More than equal 3e11 and less than or equal 4e14: Infrared
-elif frequency > 3e11 and frequency <= 4e14:
-    rad_level = INFRARED
-# More than equal 4e14 and less than or equal 7.5e14: Visible light
-elif frequency > 4e14 and frequency <= 7.5e14:
-    rad_level = VISIBLE 
-# More than equal 7.5e14 and less than or equal 3e16: Ultraviolet
-elif frequency > 7.5e14 and frequency <= 3e16:
-    rad_level = ULTRAVIOLET
-# More than equal 3e16 and less than or equal 3e19: X-Rays
-elif frequency > 3e16 and frequency <= 3e19:
-    rad_level = X_RAYS
-# More 3e19: Gamma rays
+# Read unit user
+
+unit = input("Enter 'W' for Wavelength, or 'F' for Frequency")
+if unit == 'F':
+    # Capture the frequency
+    frequency = float(input('Enter the frequency. ex: 3e12 '))
+
+    # Display the description of the radiation
+    # Less than or equal 3e9: Radio waves
+    if frequency <= 3e9: 
+        rad_level = RADIO
+    # More than 3e9 and less than or equal 3e11: Microwaves
+    elif frequency > 3e9 and frequency <= 3e11:
+        rad_level = MICROWAVE
+    # More than equal 3e11 and less than or equal 4e14: Infrared
+    elif frequency > 3e11 and frequency <= 4e14:
+        rad_level = INFRARED
+    # More than equal 4e14 and less than or equal 7.5e14: Visible light
+    elif frequency > 4e14 and frequency <= 7.5e14:
+        rad_level = VISIBLE 
+    # More than equal 7.5e14 and less than or equal 3e16: Ultraviolet
+    elif frequency > 7.5e14 and frequency <= 3e16:
+        rad_level = ULTRAVIOLET
+    # More than equal 3e16 and less than or equal 3e19: X-Rays
+    elif frequency > 3e16 and frequency <= 3e19:
+        rad_level = X_RAYS
+    # More 3e19: Gamma rays
+    else:
+        rad_level = GAMMA
+elif unit == 'W':
+    print('Support for Wavelength coming soon!')
 else:
-    rad_level = GAMMA
-
+    print('Unsupported unit')
+print(f'Based on your input, your frequency is: {RADIATION_TYPES[rad_level]}')
 
 
 # Less than or equal 3e9: Radio waves
