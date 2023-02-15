994496efc76bc8ff9db0fe7e2236a547a808dad1
Efhn <113469356+Efhn@users.noreply.github.com>
2023-02-15 19:14:16 +0000

    avg exams

---
+++ module 5/avg_exams.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+"""
+Compute the average exam grade for multiple students
+"""
+
+# TODO: Obtain the number of exam grades per student
+
+# Set your initial sentinel value
+more_grades = 'Y'
+
+# TODO: Compute average exam grades until the user wants to stop
+while_more_grades != 'Y':
+    print('Here')
+
+print('Thank you for your input')
