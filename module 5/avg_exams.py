a5f1573cbdbf76e2bb0ff8233231b2030a5fa8ad
Efhn <113469356+Efhn@users.noreply.github.com>
2023-02-15 19:34:40 +0000

    complete task

--- module 5/avg_exams.py
+++ module 5/avg_exams.py
@@ -3,13 +3,21 @@
 Compute the average exam grade for multiple students
 """
 
-# TODO: Obtain the number of exam grades per student
-
+# Obtain the number of exam grades per student
+num_exams = int(input('How many exams grades does each student have?'))
 # Set your initial sentinel value
 more_grades = 'Y'
 
 # TODO: Compute average exam grades until the user wants to stop
-while_more_grades != 'Y':
-    print('Here')
+while more_grades == 'Y':
+    print('Enter the exam grades: ')
+    total = 0
+    for exam in range(1, num_exams + 1):
+        score = int(input(f'Exam {exam}: '))  # Prompt for each exam grade
+        total = total + score 
+    # Calculate the average
+    average = total/num_exams
+    print(f'The average is {average:.2f}')
+    more_grades = input('Enter exam grades for anoother student: (Y/N)? ').upper() # make it upper case
 
 print('Thank you for your input')
